4d0448df647c618606799f850c9bfe7da0840c38
Yijun <yw1607@nyu.edu>
2019-05-21 23:27:36 -0700

    reconstructed groupInfo

--- include/GroupSummary.h
+++ include/GroupSummary.h
@@ -12,6 +12,5 @@
 #include "SuperblockSummary.h"
 
 unsigned int getBlockSize(int fd);
-// std::pair<std::vector<unsigned int>, std::vector<unsigned int>> getGroupInfo(int fd);
-// void getGroupInfo(int fd);
-std::vector<std::vector<unsigned int>> getGroupInfo(int fd);
+struct ext2_group_desc* getGroupInfo(int fd);
+// std::vector<std::vector<unsigned int>> getGroupInfo(int fd);
--- src/lab3a.cpp
+++ src/lab3a.cpp
@@ -16,7 +16,9 @@ int main()
 {
 	int fd = open("trivial.img", O_RDONLY);
 	getSuperNodeInfo(fd);
-	getGroupInfo(fd);
-	printf("Main\n");
+	struct ext2_group_desc* group_desc = getGroupInfo(fd);
+	(void) group_desc;
+	// cout << "bitmap: " << group_desc -> bg_block_bitmap << endl;
+	// printf("Main\n");
 	return 0;
 }
